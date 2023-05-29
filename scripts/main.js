652328db781e506f5c7acaa4429d5fc6d97e89c2
Paul Wiskow <usk7dg@virginia.edu>
2023-05-29 17:00:34 -0400

    Added js functionality so that when a number is clicked, it updates a map.

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+let rolls;
+
+const initialize = () => {
+    for (let dice of document.getElementsByClassName("dice")) {
+        dice.addEventListener("click", e => diceClicked(dice));
+    }
+
+    rolls = new Map([
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+        [7, 0],
+        [8, 0],
+        [9, 0],
+        [10, 0],
+        [11, 0],
+        [12, 0],
+    ]);
+}
+
+const diceClicked = (dice) => {
+    let key = Number(dice.textContent);
+    rolls.set(key, rolls.get(key) + 1);
+    console.log(rolls.get(key));
+}
